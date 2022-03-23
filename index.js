1d4494de9b22e550b2e7afe6f6f99ff3383948ff
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-03-23 16:09:48 -0500

    Âfirst push

---
+++ index.js
@@ -0,0 +1,13 @@
+// 400 14px/20px Roboto,Helvetica Neue,sans-serif;
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
